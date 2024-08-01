686d831b4f536cf6e64853f55f34b370a5aee7e1
Bittu <bittu1040@gmail.com>
2024-08-02 01:22:35 +0530

    swicthMap and mergeMap

--- src/app/components/rxjs-operator/rxjs-operator.component.ts
+++ src/app/components/rxjs-operator/rxjs-operator.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { combineLatest, forkJoin, from, interval, of, take } from 'rxjs';
-import { concat } from 'rxjs/operators';
+import { concat, delay, map, mergeMap, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs-operator',
@@ -58,6 +58,57 @@ export class RxjsOperatorComponent implements OnInit{
 
 
 
+
+    const observableA = of(1, 2, 3, 4, 5).pipe(
+      delay(1000) // Add a delay to simulate processing time
+    );
+    
+    const observableB = of(10, 20, 30, 40, 50).pipe(
+      delay(1000) // Add a longer delay
+    );
+    
+    const observableC = of(100, 200, 300, 400, 500).pipe(
+      delay(1000) // Add an even longer delay
+    );
+    
+  
+    
+    // switch map: 
+    // When it sees an inner Observable, it cancels the previous inner Observable and starts a new one.
+    const source1 = of(1, 2, 3);
+    const example1 = source1.pipe(
+      switchMap(val => of(val, val * 2, val * 3))
+    );
+     const subscribe = example1.subscribe(val => console.log(val));
+
+    const source2 = of(1, 2, 3);
+    const example2 = source2.pipe(
+    switchMap(val => interval(1000).pipe(
+    map(i => `Inner Observable ${val}: ${i}`),
+    take(3))) // Limit each inner Observable to 3 emissions
+    );
+     const subscribe2 = example2.subscribe(val => console.log("switch map",val));
+
+
+    //mergeMap
+    const source3 = of(4, 5, 3);
+    const example3 = source3.pipe(
+      mergeMap(val => of(val, val * 2, val * 3))
+    );
+    const subscribe3 = example3.subscribe(val => console.log(val));
+
+    const source4 = of(1, 2, 3);
+    const example4 = source4.pipe(
+    mergeMap(val => interval(1000).pipe(
+    map(i => `Inner Observable ${val}: ${i}`),
+    take(3)))
+    );
+    const subscribe4 = example4.subscribe(val => console.log("merge map",val));
+
   }
+
+
+
+
 
 }
