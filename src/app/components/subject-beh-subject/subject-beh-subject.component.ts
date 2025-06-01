94d7716fcbe6c441457a39299a95a7056a3774c3
Bittu <bittu1040@gmail.com>
2025-06-01 12:05:47 +0530

    added question

--- src/app/components/subject-beh-subject/subject-beh-subject.component.ts
+++ src/app/components/subject-beh-subject/subject-beh-subject.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { AsyncSubject, BehaviorSubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-subject-beh-subject',
@@ -7,12 +7,15 @@ import { BehaviorSubject, Subject } from 'rxjs';
   styleUrls: ['./subject-beh-subject.component.scss']
 })
 export class SubjectBehSubjectComponent {
-  subject= new Subject<number>();
+  subject = new Subject<number>();
   behaviorSubject = new BehaviorSubject<number>(0);
+  asyncSubject = new AsyncSubject<number>();
 
   ngOnInit() {
-    this.useSubject();
-    this.useBehaviorSubject();
+    // this.useSubject();
+    // this.useBehaviorSubject();
+    // this.useAsyncSubject();
+    this.exampleCode();
   }
 
   useSubject() {
@@ -31,6 +34,34 @@ export class SubjectBehSubjectComponent {
     this.behaviorSubject.subscribe(value => {
       console.log('BehaviorSubject value:', value);
     });
+  }
+
+  useAsyncSubject() {
+    for (let i = 0; i < 5; i++) {
+      this.asyncSubject.next(i);
+    }
+    this.asyncSubject.complete(); // Complete the AsyncSubject to emit the last value
+    this.asyncSubject.subscribe(value => {
+      console.log('AsyncSubject value:', value);
+    });
+  }
+
+  exampleCode() {
+    const subject = new Subject<number>();
+    const behaviorSubject = new BehaviorSubject<number>(0);
+    const asyncSubject = new AsyncSubject<number>();
+
+    for (let i = 1; i <= 10; i++) {
+      subject.next(i);
+      behaviorSubject.next(i);
+      asyncSubject.next(i);
+    }
+
+    subject.subscribe(value => console.log('Subject:', value));
+    behaviorSubject.subscribe(value => console.log('BehaviorSubject:', value));
+    asyncSubject.subscribe(value => console.log('AsyncSubject:', value));
+
+    asyncSubject.complete(); // Complete the subject — this is REQUIRED for AsyncSubject to emit
   }
 
 }
